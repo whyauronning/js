e8770d3fd7ad5ce6a6736af895cc9fa0bde74558
Vladimir <eyvovan@rambler.ru>
2020-04-24 02:45:41 +0300

    task part - [1]

---
+++ function.js
@@ -0,0 +1,35 @@
+// let num = 20;
+// function showFirstMessage (text){
+//     alert(text);
+//     let num = 10;
+//     console.log(num);
+// }
+// showFirstMessage("SASAI LALKA");
+// console.log(num);
+
+// function calc(a,b){
+//     returm (a+b);
+// }
+// console.log(calc(10,514));
+
+// let calc = (a,b) => a+b; // стрелочная функция
+
+// function retVar (){
+//       let num = 50;
+//       return num;
+// }
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+// преобразование свойства и методы
+
+// let str = "text"; //свойство
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase()); // методы строк
+
+// let twelve = "12.2px";
+// console.log(Math.round(twelve));
+// console.log(parseFloat(twelve)); // методы чисел
+// console.log(parseInt(twelve));
